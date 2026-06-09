071a1eadb33817cbb0ecc27f2475fee791e0bce7
utsav <utsavmishraa005@gmail.com>
2026-06-10 00:40:20 +0530

    Change self-ping to 5 minutes

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,5 @@ app.listen(port);
 if (process.env.RENDER_EXTERNAL_URL) {
   setInterval(() => {
     fetch(`${process.env.RENDER_EXTERNAL_URL}/health`).catch(() => {});
-  }, 10 * 60 * 1000);
+  }, 5 * 60 * 1000);
 }
